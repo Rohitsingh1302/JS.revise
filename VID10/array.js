a919646b84a6274c1991e98256f3d1fc507e2978
rohit1302 <rohitnew1302@gmail.com>
2025-12-25 00:00:31 +0530

    started with ARRAY

--- VID10/array.js
+++ VID10/array.js
@@ -1,21 +1,21 @@
-// let student=["rohit","bisht","shubham"]
-// console.log(student); //puri array display krdega
-// console.log(student[0]); //rohit
-// console.log(student.length) //3
-// console.log(student.indexOf("shubham")) // 2
-// student[1]="ravi"; //bisht htgya ravi ajayega
-// console.log(student);
-// student.push("rahul");//end pr add krdega 4 hogaye
-// console.log(student);
-// student.pop(); //will remove the end one
-// console.log(student);
-// student.unshift("rahul"); //will add at the start
-// console.log(student);
-// student.reverse();
-// console.log(student);
+let student=["rohit","bisht","shubham"]
+console.log(student); //puri array display krdega
+console.log(student[0]); //rohit
+console.log(student.length) //3
+console.log(student.indexOf("shubham")) // 2
+student[1]="ravi"; //bisht htgya ravi ajayega
+console.log(student);
+student.push("rahul");//end pr add krdega 4 hogaye
+console.log(student);
+student.pop(); //will remove the end one
+console.log(student);
+student.unshift("rahul"); //will add at the start
+console.log(student);
+student.reverse();
+console.log(student);
 
 
 
-// //in JS ..array could be of other datatype also
-// const myarray=[1,true,"rohit"]
-// console.log(myarray);
+//in JS ..array could be of other datatype also
+const myarray=[1,true,"rohit"]
+console.log(myarray);
